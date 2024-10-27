429834dd350237b843f8a8edcda2f3d3759dea22
Ashley Bhandari <ashleybhanda@umass.edu>
2024-10-26 23:47:46 -0400

    Account home page, private route setup

--- flote/src/app/App.tsx
+++ flote/src/app/App.tsx
@@ -1,16 +1,23 @@
-import { Routes, Route } from "react-router-dom";
+import { Navigate, Outlet, Routes, Route } from "react-router-dom";
 import Landing from "./pages/Landing";
 import SignUp from "./pages/SignUp";
 import SignIn from "./pages/SignIn";
+import AccountHome from "./pages/AccountHome";
+
+const PrivateRoutes = () => {
+  const auth = { token: true };
+  return auth.token ? <Outlet /> : <Navigate to="/sign-in" />;
+};
 
 export default function App() {
   return (
-    <div>
-      <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="sign-up" element={<SignUp />} />
-        <Route path="sign-in" element={<SignIn />} />
-      </Routes>
-    </div>
+    <Routes>
+      <Route path="/" element={<Landing />} />
+      <Route path="sign-up" element={<SignUp />} />
+      <Route path="sign-in" element={<SignIn />} />
+      <Route element={<PrivateRoutes />}>
+        <Route path="home" element={<AccountHome />} />
+      </Route>
+    </Routes>
   );
 }
---
+++ flote/src/app/pages/AccountHome.tsx
@@ -0,0 +1,3 @@
+export default function AccountHome() {
+  return <div>account home</div>;
+}
