216ae1c684855d889c390d488aea5642694949be
Austin Henlotter <ahenlotter@umass.edu>
2024-10-27 20:16:07 -0400

    integrated auth0 with react
    when users click the login button they are sent to the auth0 page, then after logging in they are sent to the home page where they can log out

--- flote/src/main.tsx
+++ flote/src/main.tsx
@@ -2,9 +2,18 @@ import { createRoot } from 'react-dom/client'
 import App from './app/App.tsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom';
+import { Auth0Provider } from '@auth0/auth0-react';
+
+
 
 createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
+  <BrowserRouter> 
+  <Auth0Provider
+	domain="dev-rvwwzp45gttpuq7a.us.auth0.com"
+    clientId="izgea8EQWehX3fGtMY0a6hM5ByiToMRb"
+	authorizationParams={{redirect_uri: window.location.origin}}
+	>
     <App />
+  </Auth0Provider>
   </BrowserRouter>,
 )
