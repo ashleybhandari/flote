e26d6d5c7265c46f4c84dde3b0da6f0316490eb1
Austin Henlotter <ahenlotter@umass.edu>
2024-10-28 14:06:04 -0400

    fixed logout to work with cookies
    before, if you had cookies logout would just log you right back in. Now it actually logs you out

--- flote/src/app/pages/AccountHome.tsx
+++ flote/src/app/pages/AccountHome.tsx
@@ -1,11 +1,14 @@
 import { useNavigate } from "react-router-dom";
-
+import { useAuth0 } from '@auth0/auth0-react';
 import OutlinedButton from "../../components/molecules/OutlinedButton";
 
 export default function AccountHome() {
   const navigate = useNavigate();
-  const handleSignOut = () => navigate("/");
-
+  const { isLoading, isAuthenticated, error, user, loginWithRedirect, logout } = useAuth0();
+  const handleSignOut = async () =>{
+	  await logout({ logoutParams: { returnTo: window.location.origin }}); 
+	  navigate("/");
+	  }
   return (
     <div className="flex flex-col items-center justify-center w-screen h-screen">
       <div className="mb-10 font-header font-bold text-[30px]">Home</div>
