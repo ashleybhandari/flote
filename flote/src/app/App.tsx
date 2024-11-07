48a62698bcf6d34abce86e7d2e3148b34e644a92
Ashley Bhandari <ashleybhanda@umass.edu>
2024-11-07 14:33:52 -0500

    Added regattas to AccountHome

--- flote/src/app/App.tsx
+++ flote/src/app/App.tsx
@@ -1,4 +1,5 @@
-import { Route, Routes } from "react-router-dom";
+import { NextUIProvider } from "@nextui-org/react";
+import { Route, Routes, useHref, useNavigate } from "react-router-dom";
 
 import { AuthenticationGuard } from "./AuthenticationGuard";
 
@@ -7,13 +8,17 @@ import Landing from "@pages/Landing";
 
 // TODO figure out why refresh is so slow
 export default function App() {
+  const navigate = useNavigate();
+
   return (
-    <Routes>
-      <Route path="/" element={<Landing />} />
-      <Route
-        path="home"
-        element={<AuthenticationGuard component={AccountHome} />}
-      />
-    </Routes>
+    <NextUIProvider navigate={navigate} useHref={useHref}>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route
+          path="home"
+          element={<AuthenticationGuard component={AccountHome} />}
+        />
+      </Routes>
+    </NextUIProvider>
   );
 }
--- flote/src/app/pages/AccountHome.tsx
+++ flote/src/app/pages/AccountHome.tsx
@@ -1,14 +1,43 @@
+import { useAuth0 } from "@auth0/auth0-react";
+import { useEffect, useState } from "react";
+
+import { EventResponse } from "@models/EventResponse";
+import { Regatta } from "@models/Regatta";
+import { socket } from "@src/socket";
+
 import AppLayout from "@templates/AppLayout";
+import RegattaList from "@atoms/RegattaList";
 import ResponsiveCard from "@molecules/ResponsiveCard";
 
 export default function AccountHome() {
+  const [regattasAdmin, setRegattasAdmin] = useState<Regatta[]>([]);
+  const [regattasTimekeeper, setRegattasTimekeeper] = useState<Regatta[]>([]);
+  const { user } = useAuth0();
+
+  useEffect(() => {
+    socket.emit("getRegattas", user?.sub, (res: EventResponse) => {
+      if (res.error) {
+        console.error("getRegattas failed:", res.error);
+      } else {
+        setRegattasAdmin(res.data.regattas.admin);
+        setRegattasTimekeeper(res.data.regattas.timekeeper);
+      }
+    });
+  }, [user]);
+
   return (
     <AppLayout className="flex flex-col md:flex-row gap-3">
       <ResponsiveCard title="my regattas">
-        list of regattas user admins
+        <RegattaList
+          ariaLabel="list of regattas you admin"
+          regattas={regattasAdmin}
+        ></RegattaList>
       </ResponsiveCard>
       <ResponsiveCard title="shared with me">
-        list of regattas user timekeeps
+        <RegattaList
+          ariaLabel="list of regattas you timekeep for"
+          regattas={regattasTimekeeper}
+        ></RegattaList>
       </ResponsiveCard>
     </AppLayout>
   );
