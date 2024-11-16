259f51e3242bc4e22e02c25a9febf03ae69d67a1
Ashley Bhandari <ashleybhanda@umass.edu>
2024-11-16 03:29:29 -0500

    Stopped footer from cutting off overflow

--- flote/src/components/templates/AppLayout.tsx
+++ flote/src/components/templates/AppLayout.tsx
@@ -12,7 +12,7 @@ export default function AppLayout({ className, children }: Props) {
   return (
     <Background className="justify-between bg-white/40">
       <Header className="px-8 lg:px-[5%] xl:px-[15%]" />
-      <main className={`h-full m-8 lg:mx-[5%] xl:mx-[15%] ${className}`}>
+      <main className={`grow m-8 lg:mx-[5%] xl:mx-[15%] ${className}`}>
         {children}
       </main>
       <FreepikAttribution />
