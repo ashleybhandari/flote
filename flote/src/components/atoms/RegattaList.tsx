654b37be941e727a09f45f861534950d069c8ca1
Ashley Bhandari <ashleybhanda@umass.edu>
2024-11-16 03:12:06 -0500

    Got rid of RegattaList scrollbars when list is short

--- flote/src/components/atoms/RegattaList.tsx
+++ flote/src/components/atoms/RegattaList.tsx
@@ -7,13 +7,12 @@ type Props = {
   regattas: Regatta[];
 };
 
-// TODO: remove scrollbars if no overflow
 export default function RegattaList({ ariaLabel, regattas }: Props) {
   return (
     <Listbox
       aria-label={ariaLabel}
       emptyContent={<p>Nothing yet!</p>}
-      classNames={{ list: "max-h-[400px] overflow-y-scroll" }}
+      classNames={{ list: "max-h-[400px] overflow-y-auto" }}
     >
       {regattas.map((r, i) => (
         <ListboxItem key={i} href={`/regatta/${r._id}`}>
