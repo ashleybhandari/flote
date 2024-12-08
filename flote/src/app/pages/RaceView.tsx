51ba63ed0a667fc606133a7cbe132f061e741697
Jamie <jdenley@umass.edu>
2024-12-08 15:57:53 -0500

    RaceView Start Time Basis

--- flote/src/app/pages/RaceView.tsx
+++ flote/src/app/pages/RaceView.tsx
@@ -11,6 +11,7 @@ import { Race } from "@models/Race";
 
 import { socket } from "@src/socket";
 import { EventResponse } from "@src/models/EventResponse";
+// import { format } from "date-fns";
 
 export default function RaceView() {
   const { raceId } = useParams();
@@ -18,6 +19,7 @@ export default function RaceView() {
 
   const [raceName, setRaceName] = useState<string>("");
   const [boats, setBoats] = useState<Boat[]>([]);
+  const [startTime, setStartTime] = useState<string>("");
 
   useEffect(() => {
     if (location.state?.race?.name) {
@@ -32,21 +34,35 @@ export default function RaceView() {
           const { race, boats: fetchedBoats } = res.data;
           setRaceName(race.name);
           setBoats(fetchedBoats);
+          if (res.data.race.startTime) {
+            setStartTime(res.data.race.startTime);
+          }
         }
       });
     }
   }, [raceId, location.state]);
 
   console.log("Current boats:", boats)
+  const participants = boats.flatMap((boat) => boat.participantNames || []);
 
   return (
     <AppLayout title={raceName} subtitle="race" className="flex">
       <div className="grow flex flex-col lg:flex-row gap-3">
+        <ResponsiveCard title="Race Start Time">
+          <p>{startTime}</p>
+        </ResponsiveCard>
         <ResponsiveCard title="Boats in Race">
           <List
             ariaLabel="List of boats"
             itemType="race"
             items={boats}
+          />
+        </ResponsiveCard>
+        <ResponsiveCard title="Participants in Race">
+          <List
+            ariaLabel="List of Participants"
+            itemType="race"
+            items={participants}
           />
         </ResponsiveCard>
       </div>
